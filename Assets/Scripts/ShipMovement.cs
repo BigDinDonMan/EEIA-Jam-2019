563453f45a183b437ece34790150281f362c5572
Maciej <gorski.maciej@o2.pl>
2019-03-23 00:13:29 +0100

    Added a couple of basic classes and basic movement using Gyroscope class

---
+++ Assets/Scripts/ShipMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ShipMode {
+	Horizontal, Vertical
+}
+
+[RequireComponent(typeof(Rigidbody))]
+public class ShipMovement : MonoBehaviour
+{
+	public Gyroscope gyro = null;
+	public Rigidbody rb = null;
+
+	public float speed;
+
+	public ShipMode movementMode;
+
+    void Start()
+    {
+    	movementMode = ShipMode.Horizontal;
+        gyro = Input.gyro;
+        gyro.enabled = true;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+    	if (Input.GetMouseButtonDown(0)) {
+    		ToggleShipMode();
+    	}
+    	switch (movementMode) {
+    		case ShipMode.Horizontal:
+		        rb.velocity = new Vector3(Input.acceleration.x, 0f, Input.acceleration.y) * Mathf.Pow(speed, 2) / 2;// * Time.deltaTime;
+    			break;
+    		case ShipMode.Vertical:
+    			rb.velocity = Vector3.zero;
+    			rb.velocity = new Vector3(rb.velocity.x, Input.acceleration.y, rb.velocity.z);
+    			break;
+    	}
+    }
+
+    void ToggleShipMode() {
+    	if (movementMode == ShipMode.Horizontal) movementMode = ShipMode.Vertical;
+    	else movementMode = ShipMode.Horizontal;
+    }
+}
---
+++ Assets/Scripts/Stats.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stats
+{
+	public ulong score;
+	public float speed;
+
+	private static Stats instance = null;
+
+	public static Stats GetInstance() {
+		if (instance == null) instance = new Stats();
+		return instance; 
+	}
+
+	private Stats() {
+		score = 0;
+	}
+}
