284014ee61695cc37105f2a059c81aa64b189c7d
Maciej <gorski.maciej@o2.pl>
2019-03-23 22:32:59 +0100

    Auto stash before merge of "master" and "origin/master"

--- Assets/Scripts/Animal.cs
+++ Assets/Scripts/Animal.cs
@@ -18,6 +18,7 @@ public class Animal : MonoBehaviour
     public bool isInBeam;
     private Quaternion basicRotation;
     private Rigidbody rb;
+    private float changeDirectionTimer = 0f;
 
     void OnDestroy() {
     	if (scriptRef.beamAnimals.Contains(this)) {
@@ -31,6 +32,7 @@ public class Animal : MonoBehaviour
     	if (player == null) player = GameObject.FindWithTag("Player").transform;
     	rb = GetComponent<Rigidbody>();
     	scriptRef = player.gameObject.GetComponent<ShipMovement>();
+    	changeDirectionTimer = Random.Range(3f, 6f);
     }
 
     void Update() {
@@ -41,6 +43,12 @@ public class Animal : MonoBehaviour
     		}
     		basicRotation = this.transform.rotation;
     		rb.useGravity = true;
+    		changeDirectionTimer -= Time.deltaTime;
+    		if (changeDirectionTimer <= 0f) {
+    			changeDirectionTimer = Random.Range(1f, 2f);
+    			this.transform.Rotate(0f, 15f, 0f);
+    		}
+    		this.transform.Translate(transform.forward * speed * Time.deltaTime);
     	} else {
     		//rb.useGravity = false;
     		/*this.transform.position = Vector3.Lerp(this.transform.position, player.transform.position, speed * Time.deltaTime);
