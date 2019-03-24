8219913e66a40cca4a4dad156f4e3e59dfba989c
Maciej <gorski.maciej@o2.pl>
2019-03-24 04:54:26 +0100

    Start of map

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -2,20 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour
 {
-	public Transform gyroInfoWindow;
-	public Transform normalWindow;
+    public Transform infoWindow;
+    public Button[] buttons;
 
     public void GameStart() => SceneManager.LoadScene(1);
     public void ExitGame() => Application.Quit();
+    public void ShowInfo() {
+        infoWindow.gameObject.SetActive(true);
+        foreach (var button in buttons) button.gameObject.SetActive(false);
+    }
+
+    public void HideInfo() {
+        infoWindow.gameObject.SetActive(false);
+        foreach (var button in buttons) button.gameObject.SetActive(true);
+    }
 
     private void Start() {
-    	if (!SystemInfo.supportsGyroscope) {
-    		gyroInfoWindow.gameObject.SetActive(true);
-    	} else {
-    		normalWindow.gameObject.SetActive(true);
-    	}
+        infoWindow.gameObject.SetActive(false);
     }
 }
