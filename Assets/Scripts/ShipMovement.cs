39230775ceb12117d6290a139a622a02e6ae8f3f
Maciej <gorski.maciej@o2.pl>
2019-03-24 01:56:44 +0100

    Added game over screen and resetting the game.

--- Assets/Scripts/ShipMovement.cs
+++ Assets/Scripts/ShipMovement.cs
@@ -1,6 +1,8 @@
-﻿using System.Collections;
+﻿#define DEBUG
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 
 public enum ShipMode {
 	Horizontal, Vertical
@@ -38,6 +40,9 @@ public class ShipMovement : MonoBehaviour
 
 	public static bool buttonPressed = false;
 
+	public static bool gameOver = false;
+	public bool savedHighScore = false;
+
 	public List<Animal> beamAnimals = new List<Animal>();
 
 
@@ -53,47 +58,70 @@ public class ShipMovement : MonoBehaviour
         beamPoint.gameObject.SetActive(false);
         beamParticles.gameObject.SetActive(false);
         movingParticles.gameObject.SetActive(true);
+        StartCoroutine("LowerFuel");
     }
 
     void Update()
     {
-    	//rb.velocity = ufoRb.velocity = Vector3.zero;
-    	switch (movementMode) {
-    		case ShipMode.Horizontal:
-		       /* rb.velocity += new Vector3(Input.acceleration.x, 0f, Input.acceleration.y) *Time.deltaTime * 3f * speed;
-		        rb.velocity = Vector3.ClampMagnitude(rb.velocity, clampValue);
-		        ufoRb.velocity = rb.velocity;
-		        RotateShip();*/
-		       	if (Input.acceleration.y > delta || Input.acceleration.y < -delta) {
-		       		rb.velocity += transform.forward * speed * Input.acceleration.y;
-		       	} else {
-		       		rb.velocity *= 0.8f;
-		       	}
-		       	if (Input.acceleration.x > delta || Input.acceleration.x < -delta) {
-		       		rb.velocity += transform.right * Input.acceleration.x * speed;
-		       	} else {
-		       		rb.velocity *= 0.8f;
-		       	}
-		       	rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
-		        rb.velocity = Vector3.ClampMagnitude(rb.velocity, clampValue);
-		        ufoRb.velocity = rb.velocity;
-		        RotateShip();
-    			break;
-    		case ShipMode.Vertical:
-    			rb.velocity = new Vector3(rb.velocity.x, Input.acceleration.y, rb.velocity.z) * speed;
-    			ufoRb.velocity = rb.velocity;
-    			ufoRb.constraints = RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationX;
-    			if (Input.GetMouseButtonDown(0)) {
-    				beamAnimals.ForEach(animal => {
-    					if (animal.isInBeam) animal.BumpUpwards(beamForce);
-    					else try{
-    						beamAnimals.Remove(animal);
-    					} catch (System.Exception) {}
-    				});
-    			}
-    			ufoRb.constraints = RigidbodyConstraints.None;
-    			break;
+    	#if DEBUG
+    		if (Input.GetMouseButtonDown(0)) {
+    			Stats.GetInstance().ModifyFuel(-150f);
+    		}
+    	#endif
+    	
+    	if (gameOver) {
+    		//Time.timeScale = 0;
+    		cam.transform.SetParent(null);
+    		cam.transform.RotateAround(this.transform.position, Vector3.up, rotateAngle * Time.deltaTime);
+    		UIManager.instance.ActivateGameOver();
+    		if (!savedHighScore) {
+    			savedHighScore = true;
+    			PlayerPrefs.SetInt("highestCount", Stats.GetInstance().money);
+    		}
+    		return;
     	}
+
+    	//rb.velocity = ufoRb.velocity = Vector3.zero;
+    	if (Stats.GetInstance().fuel > 0f) {
+	    	switch (movementMode) {
+	    		case ShipMode.Horizontal:
+			       /* rb.velocity += new Vector3(Input.acceleration.x, 0f, Input.acceleration.y) *Time.deltaTime * 3f * speed;
+			        rb.velocity = Vector3.ClampMagnitude(rb.velocity, clampValue);
+			        ufoRb.velocity = rb.velocity;
+			        RotateShip();*/
+			       	if (Input.acceleration.y > delta || Input.acceleration.y < -delta) {
+			       		rb.velocity += transform.forward * speed * Input.acceleration.y;
+			       	} else {
+			       		rb.velocity *= 0.8f;
+			       	}
+			       	if (Input.acceleration.x > delta || Input.acceleration.x < -delta) {
+			       		rb.velocity += transform.right * Input.acceleration.x * speed;
+			       	} else {
+			       		rb.velocity *= 0.8f;
+			       	}
+			       	rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+			        rb.velocity = Vector3.ClampMagnitude(rb.velocity, clampValue);
+			        ufoRb.velocity = rb.velocity;
+			        RotateShip();
+	    			break;
+	    		case ShipMode.Vertical:
+	    			rb.velocity = new Vector3(rb.velocity.x, Input.acceleration.y, rb.velocity.z) * speed;
+	    			ufoRb.velocity = rb.velocity;
+	    			ufoRb.constraints = RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezeRotationY | RigidbodyConstraints.FreezeRotationX;
+	    			if (Input.GetMouseButtonDown(0)) {
+	    				beamAnimals.ForEach(animal => {
+	    					if (animal.isInBeam) animal.BumpUpwards(beamForce);
+	    					else try{
+	    						beamAnimals.Remove(animal);
+	    					} catch (System.Exception) {}
+	    				});
+	    			}
+	    			ufoRb.constraints = RigidbodyConstraints.None;
+	    			break;
+	    	}
+	    } else {
+	    	ufoRb.useGravity = true;
+	   	}
     }
 
     public void ToggleShipMode() {
@@ -132,4 +160,16 @@ public class ShipMovement : MonoBehaviour
     void RotateShip() {
     	this.transform.rotation = Quaternion.Lerp(basicRotation,Quaternion.Euler(new Vector3(ufoRb.velocity.z, 0f, -ufoRb.velocity.x) * 1.33f), tilt);
     }   
+
+    IEnumerator LowerFuel() {
+    	while (true) {
+    		Stats.GetInstance().ModifyFuel(-1.5f);
+    		yield return new WaitForSeconds(1.5f);
+    	}
+    }
+    void OnCollisionEnter(Collision other) {
+    	if (other.gameObject.CompareTag("Ground")) {
+    		gameOver = true;
+    	}
+    }
 }
